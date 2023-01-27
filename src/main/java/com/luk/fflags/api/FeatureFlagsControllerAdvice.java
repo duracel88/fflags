8cc269c6568089a05f1f48705c9f6f0a5db86fde
Lukasz <lukasz.gawel@gmail.com>
2023-01-27 06:59:55 +0100

    Added log

--- src/main/java/com/luk/fflags/api/FeatureFlagsControllerAdvice.java
+++ src/main/java/com/luk/fflags/api/FeatureFlagsControllerAdvice.java
@@ -1,17 +1,19 @@
 package com.luk.fflags.api;
 
 import com.luk.fflags.domain.fflags.FlagNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
+@Slf4j
 public class FeatureFlagsControllerAdvice {
 
     @ExceptionHandler(FlagNotFoundException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    void handle(FlagNotFoundException e){
-
+    void handle(FlagNotFoundException e) {
+        log.debug(e.getMessage(), e);
     }
 }
